7337e00ba4cb20e1fb0fbe21e5221e760a506baf
Thamied <thamied-2300@hotmail.com>
2023-11-16 10:43:13 +0100

    few changes

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,3 +1,7 @@
+
+import getJSONfromLocalStorage from "./getJSONfromLocalStorage"
+
+
 // Function to fetch data from the API and update headlines and descriptions
 (function () {
   const categories = [
@@ -27,62 +31,50 @@
     "us",
     "world",
   ];
-  const list = document.querySelector(".news__category__list");
-  console.log(list)
-  categories.forEach((category) => {
-    list.innerHTML += `
-    <li> 
+  const list = document.querySelector(".news__category__all__list");
+  categories.forEach(async (category) => {
+    const LI = document.createElement('li')
+
+    LI.innerHTML += `
     <h2>${category}</h2>
-    </li>
-    `;
+    `
+
+    LI.addEventListener('click', async () => {
+      const apiKey = "S9jcgNFQNpvpBKGockNgkBjQQRoMaYKi";
+      const apiUrl = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${apiKey}`;
+
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      data.results.forEach(res => {
+        LI.innerHTML += `
+          ${res.title}
+          ${res.abstract}
+        `
+      })
+    })
+
+    list.append(LI)
   });
 })();
 
-
 // Function to fetch data from the API and update headlines and descriptions
-async function updateHeadlinesAndDescriptions() {
-  try {
-      const apiKey = "S9jcgNFQNpvpBKGockNgkBjQQRoMaYKi";
-      const apiUrl = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`;
-     
-      // Fetch data from the API
-      const response = await fetch(apiUrl);
-      const data = await response.json();
- 
-      // Check if data was fetched successfully
-      if (response.ok && data.results) {
-          // Loop through the articles and update headlines and descriptions
-          const articles = document.querySelectorAll('.news__category__list .headline');
-          const descriptions = document.querySelectorAll('news__category__list .description');
-         
-          for (let i = 0; i < articles.length; i++) {
-              const article = articles[i];
-              const description = descriptions[i];
-             
-              // Update headline
-              article.textContent = data.results[i]?.title || "No Title Available";
-             
-              // Update description
-              description.textContent = data.results[i]?.abstract || "No Description Available";
-          }
-      } else {
-          console.error("Error fetching data from API");
-      }
-  } catch (error) {
-      console.error("An error occurred:", error);
-  }
+async function updateHeadlinesAndDescriptions(category) {
+  const apiKey = "S9jcgNFQNpvpBKGockNgkBjQQRoMaYKi";
+  const apiUrl = `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=${apiKey}`;
+
+  // Fetch data from the API
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  // Loop through the articles and update headlines and descriptions
+
+  // Update headline
+  article.textContent = data.results[i]?.title || "No Title Available";
+
+  // Update description
+  data.results[i]?.abstract || "No Description Available";
 }
- 
+
 // Call the function to update headlines and descriptions when the page loads
-window.addEventListener('load', updateHeadlinesAndDescriptions);
- 
- 
-export default (function () {
-  fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=S9jcgNFQNpvpBKGockNgkBjQQRoMaYKi')
-    .then(response => response.json())
-    .then(data => {
- 
-      console.log(data);
- 
-    });
-})()
+window.addEventListener("load", updateHeadlinesAndDescriptions);
